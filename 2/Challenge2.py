85820cc93ab6e778fc939ba127d2d2a3b6d86bfd
nmisiuna <41637162+nmisiuna@users.noreply.github.com>
2019-11-07 12:18:04 -0500

    Update Challenge2.py

--- 2/Challenge2.py
+++ 2/Challenge2.py
@@ -8,6 +8,8 @@ This is a temporary script file.
 import pandas as pd
 pd.options.display.max_columns = 500
 import matplotlib.pyplot as plt
+import sklearn.linear_model as lm
+from datetime import datetime
 
 #Read in data
 data = pd.read_csv('C:\\Users\\Nick\\Documents\\GitHub\\DataScienceChallenges\\2\\ad_table.csv')
@@ -15,6 +17,7 @@ data = pd.read_csv('C:\\Users\\Nick\\Documents\\GitHub\\DataScienceChallenges\\2
 #Pre-process
 data['date'] = pd.to_datetime(data['date'], format = '%Y-%m-%d')
 data.sort_values('date', inplace = True)
+data['date_as_day'] = data['date'].dt.dayofyear
 data['total_cost_per_click'] = data['avg_cost_per_click'] * data['clicked']
 
 #Look at the different ad groups
@@ -29,6 +32,14 @@ print((adGroupsAgg['total_revenue']['sum'] /
 
 #Now predict through december
 plt.figure(figsize = (10, 10))
-plt.plot(data['date'], data['shown'].cumsum())
-plt.xticks(rotation = -30)
+for group in data['ad'].unique():
+    df = data.loc[data['ad'] == group, :]
+    plt.plot(df['date'], df['shown'].cumsum())
+    plt.xticks(rotation = -30)
 plt.show()
+
+line = lm.LinearRegression()
+line.fit(data['date_as_day'].values.reshape(-1, 1), 
+         data['shown'].cumsum().values.reshape(-1, 1))
+
+
