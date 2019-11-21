e97d815f94906c9dc7cc10e25ca40a3480f0c0c1
nmisiuna <41637162+nmisiuna@users.noreply.github.com>
2019-11-21 14:04:44 -0500

    Update Challenge4.py

--- 4/Challenge4.py
+++ 4/Challenge4.py
@@ -5,7 +5,7 @@ Created on Thu Nov 21 09:57:09 2019
 @author: Nick
 """
 
-import json
+#import json
 import pandas as pd
 pd.options.display.max_columns = 500
 
@@ -18,8 +18,62 @@ data = pd.read_json('C:\\Users\\Nick\\Documents\\GitHub\\DataScienceChallenges\\
 #Much better
 
 #Top 3 states by number of users
+#This does unique users
+print('Unique Users')
+print('Top three states: ')
+print(data[['user_id', 'user_state']].groupby('user_state').agg({'user_id':pd.Series.nunique}).sort_values('user_id', ascending = False)[1:4])
+print('Bottom three states: ')
+print(data[['user_id', 'user_state']].groupby('user_state').agg({'user_id':pd.Series.nunique}).sort_values('user_id', ascending = False)[-3:])
+
+#We should really look at total number of songs played per state, not unique users
+print('\nEngagement (songs played)')
 print('Top three states: ')
 print(data[['user_id', 'user_state']].groupby('user_state').agg('count').sort_values('user_id', ascending = False)[1:4])
 print('Bottom three states: ')
 print(data[['user_id', 'user_state']].groupby('user_state').agg('count').sort_values('user_id', ascending = False)[-3:])
 
+#Let's briefly look at number of songs played per user
+data['user_id'].value_counts().hist()
+#Doesn't seem too unexpected.  Let's move on
+
+#Return a list of the first users who signed up in each state
+print('\nFirst user in each state')
+print(data.sort_values('user_sign_up_date').drop_duplicates(subset = 'user_state'))
+
+#Function that returns the most likely song to be played after a song
+
+def NextSong(currSong):
+    #First get all the data that goes with this song
+#    df = data.loc[data['song_played'] == currSong, :]
+    
+    #This is the most  memory expensive way to do this
+    df = data.sort_values(by = ['user_id', 'time_played'])
+    df.reset_index(inplace = True)
+    #Get the index of all the song
+    indices = df.loc[df['song_played'] == currSong, :].index
+    #Then look at index + 1 (since we've sorted on time_played)
+    #ensuring the user_id is the same as that in index
+    temp = {} #Dictionary will be a histogram of songs played
+    for i in indices:
+        if df.loc[i + 1, 'user_id'] == df.loc[i, 'user_id']:
+            song = df.loc[i + 1, 'song_played']
+            if song in temp.keys():
+                temp[song] += 1
+            else:
+                temp[song] = 1
+    #if empty dictionary just pick any song randomly from the overall distribution
+    
+    if not temp:
+        return(df['song_played'].sample(n = 1).values[0]) #This is inefficient
+        #Better way would be to keep a histogram of songs and update it real time
+        #Then just sample from that
+    else:
+        return(list(temp.keys())[list(temp.values()).index(max(list(temp.values())))])
+    
+#test it out
+print('Testing most likely next song')
+print('Current song = Hey Judge')
+print('Next song = ' + NextSong('Hey Jude'))
+#Things to consider that I'm not doing:
+#1. Should not consider next song if time different between next and current is too large
+#2. Returning the same song can lead to an infinite loop if this is actually used for recommendations
